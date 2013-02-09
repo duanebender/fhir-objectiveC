9f8df0b9702a26d95874f8bbec4222d20b18eabc
Duane Bender <duane.bender@mohawkcollege.ca>
2013-02-09 11:56:55 -0500

    we need to change the copyright notice - i will give you the standard mohawk one - it will be an apache 2 license

--- Patient.h
+++ Patient.h
@@ -3,7 +3,7 @@
 //  FHIR
 //
 //  Created by Adam Sippel on 2013-01-24.
-//  Copyright (c) 2013 Adam Sippel. All rights reserved.
+//  Copyright (c) 2013 Mohawk College. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
