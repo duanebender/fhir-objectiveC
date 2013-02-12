df11d53403daf411b1e4c383ca21a7456ef35be5
Adam Sippel <adam.sippel@mohawkcollege.ca>
2013-02-12 11:55:42 -0500

    Fixed Linker errors caused during testing.

---
+++ FHIR/Type.m
@@ -0,0 +1,13 @@
+//
+//  Type1.m
+//  FHIR
+//
+//  Created by Adam Sippel on 2013-02-12.
+//  Copyright (c) 2013 Adam Sippel. All rights reserved.
+//
+
+#import "Type.h"
+
+@implementation Type
+
+@end
--- FHIRTests/FHIRTests.m
+++ FHIRTests/FHIRTests.m
@@ -51,30 +51,29 @@
     //activestatus test
     patient.active = YES;
     NSLog(@"Active Status of Patient: %@", ([patient active])? @"YES" : @"NO");
-    STAssertTrue(patient.active == YES, @"BOOL Should be YES");
-    STAssertTrue(patient.active == NO, @"BOOL Should be YES, this test should fail.");
+    STAssertTrue(patient.active == YES, @"BOOL Should be YES"); //should pass
+    STAssertTrue(patient.active == NO, @"BOOL Should be YES, this test should fail."); //should fail
     
     //test resoourcetype
-    STAssertTrue(patient.getResourceType == ResourceTypePatient, @"Should be true.");
-    STAssertTrue(patient.getResourceType == ResourceTypeAdmission, @"Should fail, patient should be of patient type.");
+    STAssertTrue(patient.getResourceType == ResourceTypePatient, @"Should be true."); //should pass
+    STAssertTrue(patient.getResourceType == ResourceTypeAdmission, @"Should fail, patient should be of patient type."); //should fail
     
     //test maritalstatus
     patient.details.maritalStatus.text.value = @"Single";
-    STAssertTrue(patient.details.maritalStatus.text.value == @"Married", @"Should be single.");
+    STAssertTrue(patient.details.maritalStatus.text.value == @"Married", @"Should be single."); //should fail
     
     //test animal
     Coding *patientCode = [[Coding alloc] init];
     patientCode.code.codeText = @"This is code apparently";
     [patient.animal.breed.coding addObject:patientCode];
-    Coding *testableValue = [patient.animal.breed.coding objectAtIndex:0];
-    STAssertTrue(testableValue.code.codeText == @"This is code apparently" , @"This should pass.");
+    Coding *testableValue = [patient.animal.breed.coding objectAtIndex:1];
+    STAssertTrue([testableValue.code.codeText caseInsensitiveCompare:@"This is code apparently"], @"This should pass."); //should pass
     
-#warning - commented code causes linker errors...not sure why atm
-    //ResourceReference *resource = [[ResourceReference alloc] init];
-    //HumanId *human1 = [[HumanId alloc] init];
-    //human1.period.start = [NSDate date];
-    //[patient.identifier addObject:human1];
-    //STAssertTrue(human1.period.start == [NSDate date], @"Dates should match.");
+    //identifier testing
+    HumanId *human1 = [[HumanId alloc] init];
+    human1.period.start = [NSDate date];
+    [patient.identifier addObject:human1];
+    STAssertTrue(human1.period.start == [NSDate date], @"Dates should match."); //should pass
 
     
 }
