214f7e89e813f0a555f8e89a50b4ca32027eabd0
Adam Sippel <adam.sippel@mohawkcollege.ca>
2013-04-18 15:57:24 -0400

    Builds to a usable FHIR library.a

---
+++ FHIR.h
@@ -0,0 +1,13 @@
+//
+//  FHIR.h
+//  FHIR
+//
+//  Created by Adam Sippel on 2013-04-18.
+//  Copyright (c) 2013 Mohawk College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FHIR <NSObject>
+
+@end
---
+++ libFHIR.h
@@ -0,0 +1,13 @@
+//
+//  libFHIR.h
+//  FHIR
+//
+//  Created by Adam Sippel on 2013-04-18.
+//  Copyright (c) 2013 Mohawk College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libFHIR : NSObject
+
+@end
